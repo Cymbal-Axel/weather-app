e42356bc567d903e01911c0b44cd16d5cd220056
Axel <cymbalaxel@gmail.com>
2022-10-13 19:34:20 -0300

    se hizo un mapeo de las cities recibidas por props en Cards, renderizando cada citie con el componente Card

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import Card from './Card.jsx';
 export default function Cards(props){
-
     if(!props.cities){
         return <h1>No hay Ciudades</h1>
     }
-    return <div>Cards component</div>
+    return (
+        <div>
+            {
+                props.cities && props.cities.map(city => (
+                    <Card 
+                        max={city.main.temp_max}
+                        min={city.main.temp_min}
+                        name={city.name}
+                        img={city.weather[0].icon}
+                        onClose={()=> alert(city.name)}
+                    />
+                ))
+            }
+        </div>
+    )
 
 }
