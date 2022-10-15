65105649cd83c8dce08c4ce84408cf3574b87e3c
Axel <cymbalaxel@gmail.com>
2022-10-15 12:09:14 -0300

    se agrego un button

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,6 +4,7 @@ export default function SearchBar(props){
     return (
         <div>
             <input type="text" placeholder="Ciudad"></input>
+            <button>Agregar</button>
         </div>
     )
 }
