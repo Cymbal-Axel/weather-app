4cd85abf8dc8246dbf5bcb3db16d210c7352a0df
Axel <cymbalaxel@gmail.com>
2022-10-22 23:04:25 -0300

    se agrego la carpeta de estilos para los componentes y se le dio estilo al boton de cerrar con un background red

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import s from '../styles/Card.module.css';
 
 export default function Card(props){
-console.log(props)
     return (
     <div>
-        <button onClick={props.onClose}>X</button>
+        <button onClick={props.onClose} className={s.btn}>X</button>
         <h4>{props.name}</h4>
         <div>
             <p>Min</p>
