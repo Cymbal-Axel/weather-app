49157ef15676fbdefdb12ae1cd9b0e310776d12c
Axel <cymbalaxel@gmail.com>
2022-10-27 21:36:48 -0300

    se acomodo la data dentro de las cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,12 +5,16 @@ export default function Card(props){
     return (
     <div className={s.card}>
         <button onClick={props.onClose} className={`${s.btn} ${s.btnColor}`}>X</button>
-        <h4>{props.name}</h4>
+        <h4 className={s.cardName}>{props.name}</h4>
         <div className={s.middleDiv}>
-            <p>Min</p>
-            <p>{props.min}</p>
-            <p>Max</p>
-            <p>{props.max}</p>
+            <div className={s.temperatureDiv}>
+                <p>Min</p>
+                <p>{props.min}</p>
+            </div>
+            <div className={s.temperatureDiv}>
+                <p>Max</p>
+                <p>{props.max}</p>
+            </div>
         </div>
         <img src={`http://openweathermap.org/img/wn/${props.img}@2x.png`}/>
     </div>
