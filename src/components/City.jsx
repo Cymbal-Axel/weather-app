ecc27c2e9986714625eb6a6987adfb03415d031c
Axel <cymbalaxel@gmail.com>
2022-11-11 21:40:11 -0300

    se creo componente City

---
+++ src/components/City.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+export default function City(){
+    return(
+        <div>soy una city</div>
+    )
+}
+
+
+
+/*
+                    -----COMPONENTE DE CLASE-----
+
+class City extends React.component{
+    //Constructor cuando quiero definir un estado!
+    //constructor(props){
+        //super(props)
+        //this.state = {}
+
+    render(){
+        return (
+            <div>soy una city</div>
+        )
+    }
+}
+export default City;*/
+
