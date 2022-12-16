7387b0cc2342b1e4d5e4f5f62972edd189899a0d
Axel <cymbalaxel@gmail.com>
2022-12-16 16:49:30 -0300

    se posiciono el search al final del navBar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import SearchBar from './SearchBar.jsx';
 import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 
 function Nav({onSearch}){
     return(
         <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">Weather App</Navbar.Brand>
+            <Container fluid>
+                <Navbar.Brand href="#home">Weather App</Navbar.Brand>
         
-            <SearchBar onSearch={onSearch}/>
-        
+                <SearchBar onSearch={onSearch}/>
+            </Container>
         </Navbar>
     )
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,13 +6,13 @@ export default function SearchBar({onSearch}){
     const [city, setCity] = useState("");
 
     return (
-        <form className={styles.searchContainer} onSubmit={(e)=> {
+        <form className="justify-content-end" onSubmit={(e)=> {
             e.preventDefault(); //va a prevenir que se recarge la pagina, por el onSubmit
             onSearch(city);
             setCity(""); //para que se limpie el input luego de hacer la busqueda.
         }}>
-            <input type='text' placeholder='Ciudad...' value={city} onChange={e => setCity(e.target.value)}/>
-            <input type='submit' value='Agregar'/>
+            <input className={styles.btnSize} type='text' placeholder='Ciudad...' value={city} onChange={e => setCity(e.target.value)}/>
+            <input className={styles.btn} type='submit' value='Agregar'/>
         </form>
 
     );
