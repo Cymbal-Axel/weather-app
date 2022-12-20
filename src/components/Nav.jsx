c5edfd0d71dee5b11c91ffa0de551d4c6a980448
Axel <cymbalaxel@gmail.com>
2022-12-20 16:56:28 -0300

    se agrego un Link To para el Componente About

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import SearchBar from './SearchBar.jsx';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
+import {Link} from 'react-router-dom';
 
 function Nav({onSearch}){
     return(
         <Navbar bg="light" expand="lg">
             <Container fluid>
-                <Navbar.Brand href="#home">Weather App</Navbar.Brand>
-        
+                <Link to={'/'}>
+                    <Navbar.Brand href="#home">Weather App</Navbar.Brand>
+                </Link>
+
+                <Link to={'/about'}>About</Link>
+
                 <SearchBar onSearch={onSearch}/>
             </Container>
         </Navbar>
