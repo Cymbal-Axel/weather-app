e9c856314e03610dc7723b89f71d1550a5407fa1
Axel <cymbalaxel@gmail.com>
2022-12-30 17:14:19 -0300

    se agrego un comentario en la linea 8

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,31 +1,38 @@
 import React from 'react';
+import Nav from './Nav.jsx'
+import {useParams} from 'react-router-dom';
 
 
 export default function City(props){
-    if(!props){
-        return(<div>La ciudad no existe</div>)
-    }
-    return (
-    <div>
+    let params = useParams();
+    let citiId = parseInt(params.cityId) // puse parseInt porque el ID recibido por URL viene como un STRING
 
-        <div>ID: {props.cities[0].id}</div>
-        <div>Nombre: {props.cities[0].name}</div>
-        <div>Temperatura maxima: {props.cities[0].max}</div>
-        <div>Temperatura minima: {props.cities[0].min}</div>
-        <div>Latitud: {props.cities[0].latitude}</div>
-        <div>Longitud: {props.cities[0].longitude}</div>
+    let city = props.cities.filter((c)=> c.id === citiId);
+    console.log(city[0])
+    return (<div>
+        <div>ID: {city[0].id}</div>
+        <div>Nombre: {city[0].name}</div>
+        <div>Temperatura maxima: {city[0].max}</div>
+        <div>Temperatura minima: {city[0].min}</div>
+        <div>Latitud: {city[0].latitude}</div>
+        <div>Longitud: {city[0].longitude}</div>
+        <div>Viento: {city[0].wind}</div>
 
-    </div>)
+     </div>)
 
 
 
-    /*let params = useParams();
-    return(
-        <div>soy la city numero:{params.cityId}</div>
-    )*/
 }
 
 
+{/*function onFilter(citieId){
+        let city = cities.filter((c)=> c.id === citieId);
+        if (city.length > 0){
+          return city[0];
+        } else {
+          return null;
+        }
+      }*/}
 
 /*
                     -----COMPONENTE DE CLASE-----
