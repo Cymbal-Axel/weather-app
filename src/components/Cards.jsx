6a2405eab58a4d42f17642efc32fea025dd8e9c3
Axel <cymbalaxel@gmail.com>
2023-01-17 20:23:14 -0300

    se importo Container, Row, Col de bootstrap para aplicarselo en donde se renderiza el componente Card para darle cierto estilo al orden de las cartas

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,12 +1,55 @@
 import React from 'react';
 import Card from './Card.jsx';
 import styles from '../styles/Cards.module.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Cards(props){
-    console.log(props)
     if(!props.cities){
         return <h1>No hay Ciudades</h1>
     }
+
+
+    return (
+        <div>
+    <Container>
+        <Row className={styles.rcontainer}>
+            
+                <Col className={styles.child}>
+                
+                    {
+                        props.cities && props.cities.map(city => (
+                            <Card
+                                key={city.id}
+                                max={city.max}
+                                min={city.min}
+                                name={city.name}
+                                img={city.img}
+                                id={city.id}
+                                onClose={()=>props.onClose(city.id)}
+                            />
+                        ))
+
+                    }
+                </Col>
+            
+        
+        </Row>
+    </Container>
+    </div>
+    )
+/* 
+import React from 'react';
+import Card from './Card.jsx';
+import styles from '../styles/Cards.module.css';
+
+export default function Cards(props){
+    if(!props.cities){
+        return <h1>No hay Ciudades</h1>
+    }
+
+
     return (
         <div className={styles.container}>
             {
@@ -21,8 +64,9 @@ export default function Cards(props){
                         onClose={()=>props.onClose(city.id)}
                     />
                 ))
+
             }
         </div>
     )
-
+*/
 }
